1426f479847f275b68e1a1165c79ec449271da18
offcn-java <test@qq.com>
2019-08-09 15:31:39 +0800

    作者：lt 内容：测试1.Java

---
+++ 1.java
@@ -0,0 +1,21 @@
+package com.offcn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author lt
+ * @create 2019-08-05 10:09
+ */
+
+//配置类
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/myPic/**").addResourceLocations("file:D:\\0311youlexuan\\myImp\\");
+    }
+}
